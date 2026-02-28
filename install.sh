6d91813c58cc6e929755b56184f8d49b1b5bc20b
AHK Province <81999063+sookolin@users.noreply.github.com>
2026-02-28 23:17:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ read_input_yn() {
 }
 
 REPO_URL="https://github.com/sookolin/SVPN.git"
-PROJECT_DIR="3xui-shopbot"
+PROJECT_DIR="SVPN"
 NGINX_CONF_FILE="/etc/nginx/sites-available/${PROJECT_DIR}.conf"
 
 echo -e "${GREEN}--- Запуск скрипта установки/обновления 3xui-ShopBot ---${NC}"
